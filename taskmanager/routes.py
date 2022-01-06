50257fbbcf92115073c5ed222214d6b82d9345ac
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-01-06 22:09:33 +0000

    add additional tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
-from taskmanager.models import Category, Task
+from taskmanager.models import Category, Task 
 
 
 @app.route("/")
@@ -8,3 +8,17 @@ def home():
     return render_template("tasks.html")
 
 
+@app.route("/categories")
+def categories():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories) # first categories is the html named file and the second categories is the name of the list above this line
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
